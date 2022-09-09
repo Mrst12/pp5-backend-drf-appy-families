f05a3a1f0ffa69d39781f44ac0fdbd1800853524
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-09 18:34:29 +0000

    Import user model to use with custom model, create profile database model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
+''' profiles database '''
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# some code provided by DRF-API walkthrough
+# modifications made for this project
+
+
+class Profile(models.Model):
+    '''Profile model for database'''
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../default_profile_x5mrcs',
+    )
+    bio = models.TextField(blank=True)
