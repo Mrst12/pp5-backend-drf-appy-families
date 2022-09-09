12f4a2c05113b2b2b71a8bb3f80d336d4db62d31
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-09 18:48:04 +0000

    Remove username from database models as may be confusing to the end user, create the meta class for ordering and the dunder string to show what to display

--- profiles/models.py
+++ profiles/models.py
@@ -9,10 +9,17 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     '''Profile model for database'''
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=50, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(
         upload_to='images/',
         default='../default_profile_x5mrcs',
     )
     bio = models.TextField(blank=True)
+
+    class Meta:
+        ''' displaying newest first profile '''
+        ordering = ['-created_on']
+
+    def __str__(self):
+        ''' shows what to display '''
+        return f"{self.owner}'s profile."
