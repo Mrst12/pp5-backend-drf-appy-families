8a39e2b0cbf2c0c66c4e64db6785e6419add4c0f
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-09 18:58:48 +0000

    Import signals and define function so a profile is created for each user that is created

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 ''' profiles database '''
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+
 
 # some code provided by DRF-API walkthrough
 # modifications made for this project
@@ -23,3 +25,13 @@ class Profile(models.Model):
     def __str__(self):
         ''' shows what to display '''
         return f"{self.owner}'s profile."
+
+
+# function taken from DRF-API walkthrough
+def create_profile(sender, instance, created, **kwargs):
+    ''' ensure a profile is created for each user created '''
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
