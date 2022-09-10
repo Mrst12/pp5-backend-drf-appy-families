9c01cbc0fd730ddc8affd7c4a92029d3b9ee7706
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-10 13:16:41 +0000

    create the serializers.py file in the profiles app and define the profile serializer ready for use

---
+++ profiles/serializers.py
@@ -0,0 +1,16 @@
+''' serializers for profile app '''
+from rest_framework import serializers
+from .models import Profile
+
+
+# class taken from DRF-APi walkthrough with modifications
+class ProfileSerializer(serializers.ModelSerializer):
+    '''profile serializer class'''
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        '''fields we want to display'''
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_on', 'bio', 'image',
+        ]
