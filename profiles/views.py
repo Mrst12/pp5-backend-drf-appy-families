9d622ee9e9f32d6b82fb67e4a02a759706d27961
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-10 12:58:28 +0000

    create view for the profile list in views.py after importing apiview, and response from the drf, also import profile model

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+''' views for profile app '''
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+
+''' class taken from DRF_API walkthrough '''
+
+
+class ProfileList(APIView):
+    ''' create profile list '''
+    def get(self, request):
+        '''get method'''
+        profiles = Profile.objects.all()
+        return Response(profiles)
