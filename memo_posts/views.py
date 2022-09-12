273d7c8ef736607ede719ec2879ab31f3e9f3c06
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-12 14:01:24 +0000

    Create the post part of the view to allow users to add content, add permissions so only the logged in user can add the memo

--- memo_posts/views.py
+++ memo_posts/views.py
@@ -1,5 +1,5 @@
 ''' views file for memo posts '''
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Memo
@@ -9,6 +9,11 @@ from .serializers import MemoSerializer
 # class taken from DRF_API walkthrough with modifications
 class MemoList(APIView):
     ''' create memo post list view '''
+    serializer_class = MemoSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         ''' get the memo posts to the view'''
         memo_post = Memo.objects.all()
@@ -16,3 +21,17 @@ class MemoList(APIView):
             memo_post, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        '''allow users to create memo posts'''
+        serializer = MemoSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
