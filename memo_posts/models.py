df328e99f37a94d5ad9768f063b5a42f8dd752cb
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-12 11:19:03 +0000

    Create the database model for memo posts, and migrate it

--- memo_posts/models.py
+++ memo_posts/models.py
@@ -1,3 +1,20 @@
+''' database model for memo posts '''
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# class taken from DRF_API walkthrough modifications made
+class Memo(models.Model):
+    ''' memo posts model '''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    For = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+
+    class Meta:
+        '''order by created on field'''
+        ordering = ['created_on']
+
+        def __str__(self):
+            ''' shows what to display '''
+            return f'{self.id} {self.For}'
