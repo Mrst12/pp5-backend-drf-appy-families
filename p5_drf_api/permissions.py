39ee41832bab2fcfcea66ef4707591939d03b7e9
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-12 09:59:28 +0000

    create permissions file, and create a custom permissions class that can be reused to check if the user is the owner

---
+++ p5_drf_api/permissions.py
@@ -0,0 +1,11 @@
+'''permissions so only logged in can edit'''
+from rest_framework import permissions
+
+
+# class taken for DRF-API walkthrough
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    ''' make sure the user can edit only '''
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
