4c52fb1460753749a8de2fb7ddb18f73c18619f8
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-13 13:03:13 +0000

    Add remaining tests for memo_posts, including cannot retrieve memo with invalid id, being able to update if they own the memo,and not being able to update if they dont own the memo

--- memo_posts/tests.py
+++ memo_posts/tests.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Memo
 
-"""
+
 # taken from DRF_API walkthrough with modifications
 class MemoListViewTests(APITestCase):
     '''test the memo list view'''
@@ -29,9 +29,11 @@ class MemoListViewTests(APITestCase):
 
     def test_user_not_logged_in_cannot_create_memo(self):
         '''test for non logged in user unable to post memos'''
-        response = self.client.post('/memo_posts/', {'content': 'testing again'})
+        response = self.client.post(
+            '/memo_posts/', {'content': 'testing again'}
+        )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-"""
+
 
 class MemoDetailViewTests(APITestCase):
     '''retrieving and updating memos'''
@@ -51,3 +53,26 @@ class MemoDetailViewTests(APITestCase):
         response = self.client.get('/memo_posts/1/')
         self.assertEqual(response.data['content'], 'lisas content')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_memo_with_invalid_id(self):
+        '''cannot get a memo with invalid id'''
+        response = self.client.get('/memo_posts/99/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_memo(self):
+        '''can an owner update their memo'''
+        self.client.login(username='lisa', password='pass')
+        response = self.client.put(
+            '/memo_posts/1/', {'content': 'new content'}
+        )
+        memo = Memo.objects.filter(pk=1).first()
+        self.assertEqual(memo.content, 'new content')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_update_memo_they_dont_own(self):
+        '''check a user cant update someone elses memo'''
+        self.client.login(username='lisa', password='pass')
+        response = self.client.put(
+            '/memo_posts/2/', {'content': 'different text'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
