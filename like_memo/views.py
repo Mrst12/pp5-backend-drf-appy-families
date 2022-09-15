22ac22c35da7064464d1e4c02583ddbca76b2633
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-15 09:57:21 +0000

    Create the memo likes list to allow the user to like a memo post

--- like_memo/views.py
+++ like_memo/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+'''views for memo posts likes app'''
+from rest_framework import generics, permissions
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from like_memo.models import MemoLikes
+from like_memo.serializers import MemoLikesSerializer
 
-# Create your views here.
+
+# Class taken from DRF_API walkthrough with modifications
+class MemoLikesList(generics.ListCreateAPIView):
+    '''list likes or create a like if logged in'''
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = MemoLikesSerializer
+    queryset = MemoLikes.objects.all()
+
+    def perform_create(self, serializer):
+        '''create like'''
+        serializer.save(owner=self.request.user)
