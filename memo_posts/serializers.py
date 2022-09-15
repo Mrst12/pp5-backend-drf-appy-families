d8eff220ebd3fa43a83a9601d55f37a04ca156c4
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-15 10:54:15 +0000

    Add the get like id function to the memo posts to allow users to retrieve their own posts to enable them to unlike

--- memo_posts/serializers.py
+++ memo_posts/serializers.py
@@ -1,5 +1,6 @@
 '''  serializers for memo posts app'''
 from rest_framework import serializers
+from like_memo.models import MemoLikes
 from .models import Memo
 
 
@@ -10,16 +11,27 @@ class MemoSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         '''check user is owner'''
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        '''show which user likes memo post so can be unliked by owner'''
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = MemoLikes.objects.filter(
+                owner=user, memo_post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         ''' fields we want to display '''
         model = Memo
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'created_on', 'content',
+            'created_on', 'content', 'like_id',
         ]
