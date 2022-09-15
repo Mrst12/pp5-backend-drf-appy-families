e7c399c8444a40dc1efe6ffce247be84533841aa
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-15 09:27:32 +0000

    create the database model for likes for memo posts, the order to show the likes, and what to show, migrate the database

--- like_memo/models.py
+++ like_memo/models.py
@@ -1,3 +1,23 @@
+'''models page for likes for the memo posts'''
 from django.db import models
+from django.contrib.auth.models import User
+from memo_posts.models import Memo
 
-# Create your models here.
+
+# class taken from DRF_walkthrough challenge with modifications
+class MemoLikes(models.Model):
+    '''class to allow user to like memo posts'''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    memo_post = models.ForeignKey(
+        Memo, related_name='like_memo', on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        '''how to order'''
+        ordering = ['-created_on']
+        unique_together = ['owner', 'memo_post']
+
+    def __str__(self):
+        '''what to return'''
+        return f'{self.owner} {self.memo_post}'
