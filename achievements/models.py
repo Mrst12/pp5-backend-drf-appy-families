9bd433aa641df6e2785076fc5c1809ba02c90f1f
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-16 09:49:08 +0000

    Add database model to models.py for achievements page, and migrate

--- achievements/models.py
+++ achievements/models.py
@@ -1,3 +1,23 @@
+'''database models for achievements page'''
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Achievements(models.Model):
+    '''database model for achievements page'''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../achievement-image_e4ii4f',
+        blank=True
+    )
+
+    class Meta:
+        ''' how to order'''
+        ordering = ['-date_created']
+
+    def __str__(self):
+        '''what to return'''
+        return f'{self.id} {self.title}'
