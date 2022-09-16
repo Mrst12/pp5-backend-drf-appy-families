65d86b41250befa4724a08dde4865731ebc39d8d
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-16 10:31:31 +0000

    create the achievement list view in views.py to list and create achievements

--- achievements/views.py
+++ achievements/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+'''views file for achievements app'''
+from rest_framework import generics, permissions
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Achievements
+from .serializers import AchievementSerializer
 
-# Create your views here.
+
+class AchievementList(generics.ListCreateAPIView):
+    '''list achievements and create achievement if logged in'''
+    serializer_class = AchievementSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Achievements.objects.all()
+
+    def perform_create(self, serializer):
+        '''associate achievement with logged in user'''
+        serializer.save(owner=self.request.user)
