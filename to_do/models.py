d27b1daf62eaf7e99aa28f2486339018d0bf24f7
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-19 18:40:59 +0000

    add database model for the todo app and migrate the database

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,28 @@
+'''database models file for the to_do page'''
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    '''Todo model for the Todo page'''
+    status_choices = [
+        ('pending', 'Pending'),
+        ('started', 'Started'),
+        ('done', 'Done')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    task_title = models.CharField(max_length=250)
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=20, choices=status_choices, default='pending'
+    )
+    urgent = models.BooleanField()
+
+    class Meta:
+        '''how to order'''
+        ordering = ['-created_on']
+
+    def __str__(self):
+        '''what to display'''
+        return f'{self.task_title}'
