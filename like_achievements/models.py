7c1b03cdc0f455e645151bfcfe355be5522b5eb0
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-19 12:00:52 +0000

    create the database model for the likes for achievements including the meta class and dunder string

--- like_achievements/models.py
+++ like_achievements/models.py
@@ -1,3 +1,23 @@
+'''database model for the likes for achievement page'''
 from django.db import models
+from django.contrib.auth.models import User
+from achievements.models import Achievements
 
-# Create your models here.
+
+class AchievementLikes(models.Model):
+    '''like model for the achievements page'''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    achievement_post = models.ForeignKey(
+        Achievements, related_name='like_achievements',
+        on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        '''how to order and user cant like same post twice'''
+        ordering = ['-created_on']
+        unique_together = ['owner', 'achievement_post']
+
+    def __str__(self):
+        '''what to display'''
+        return f'{self.owner} {self.achievement_post}'
