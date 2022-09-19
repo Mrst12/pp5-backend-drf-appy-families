e3c0650cecaaea9a35708a50a6bbc57adcf5a672
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-19 12:18:13 +0000

    add the create class, import integrity error from django db to stop the user from liking the same post twice

--- like_achievements/serializers.py
+++ like_achievements/serializers.py
@@ -1,4 +1,5 @@
 '''serializers file for the likes for achievements page'''
+from django.db import IntegrityError
 from rest_framework import serializers
 from like_achievements.models import AchievementLikes
 
@@ -13,3 +14,12 @@ class AchievementLikesSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'created_on', 'owner', 'achievement_post'
         ]
+
+    def create(self, validated_data):
+        '''stopping integrity errors'''
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
