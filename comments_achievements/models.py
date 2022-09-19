c009acccced427768247c02150beafd542f7e8db
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-19 09:48:30 +0000

    Create the database model for comments for the achievements page and migrate it

--- comments_achievements/models.py
+++ comments_achievements/models.py
@@ -1,3 +1,22 @@
+'''database model for comments for the achievements page'''
 from django.db import models
+from django.contrib.auth.models import User
+from achievements.models import Achievements
 
-# Create your models here.
+
+class AchievementsComment(models.Model):
+    '''model for comments for achievements page'''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    achievement_post = models.ForeignKey(
+        Achievements, on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    class Meta:
+        '''how to order content'''
+        ordering = ['-created_on']
+
+    def __str__(self):
+        '''what to display'''
+        return self.content
