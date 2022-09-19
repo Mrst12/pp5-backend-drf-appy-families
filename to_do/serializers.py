6553759183aec69bf1f3ce71b77e66a5ec4b460f
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-19 19:03:53 +0000

    Create serializer.py for the Todo app and create the serializer class for it including checking the user is the owner

---
+++ to_do/serializers.py
@@ -0,0 +1,24 @@
+'''serializer for todo page'''
+from rest_framework import serializers
+from to_do.models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    '''serializer for the Todo app'''
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        '''check user is owner'''
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        '''which fields to display'''
+        model = Todo
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'created_on', 'task_title', 'due_date', 'urgent', 'status',
+        ]
