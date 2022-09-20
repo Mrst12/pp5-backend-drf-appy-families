e7ddcdc7e42cb4bdec2bd01372b785abb243e0c9
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-20 10:50:16 +0000

    make urgent on database null = true as tests will not pass

--- to_do/models.py
+++ to_do/models.py
@@ -17,7 +17,7 @@ class Todo(models.Model):
     status = models.CharField(
         max_length=20, choices=status_choices, default='pending'
     )
-    urgent = models.BooleanField(blank=True)
+    urgent = models.BooleanField(null=True)
 
     class Meta:
         '''how to order'''
