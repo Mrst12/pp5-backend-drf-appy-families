e511e05d60a88f99454ebc02399073736e80ea66
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-20 11:06:51 +0000

    add testing for the list view of the todo app to test if all tasks can be listed, a logged in user can create a task, and a logged out user cannot create a task

--- to_do/tests.py
+++ to_do/tests.py
@@ -17,3 +17,16 @@ class TodoListViewTests(APITestCase):
         Todo.objects.create(owner=lisa, task_title='lisas title')
         response = self.client.get('/to_do/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_create_task(self):
+        '''check a task can be created if logged in'''
+        self.client.login(username='lisa', password='pass')
+        response = self.client.post('/to_do/', {'task_title': 'lisas title'})
+        count = Todo.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cannot_create_task(self):
+        '''test task cannot be created if user not logged in'''
+        response = self.client.post('/to_do/', {'task_title': 'lisas title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
