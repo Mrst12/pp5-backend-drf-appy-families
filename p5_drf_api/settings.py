f276fb8a1631b512d0bdfbe784a244401ccdba5b
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-22 09:16:02 +0000

    commented out cors allowed origins as failing to deploy on heroku

--- p5_drf_api/settings.py
+++ p5_drf_api/settings.py
@@ -73,7 +73,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
-CORS_ALLOWED_ORIGINS = ["https://appy-families.herokuapp.com/"]
+# CORS_ALLOWED_ORIGINS = ["https://appy-families.herokuapp.com/"]
 CORS_ALLOW_CREDENTIALS = True
 
 
