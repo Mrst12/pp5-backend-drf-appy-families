92bb8a4be6135c5168c0aef3ec24021948840cc6
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-29 12:57:09 +0000

    comment out the client origin devpart

--- p5_drf_api/settings.py
+++ p5_drf_api/settings.py
@@ -70,13 +70,14 @@ ALLOWED_HOSTS = [
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('CLIENT_ORIGIN_DEV')
     ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+# if 'CLIENT_ORIGIN_DEV' in os.environ:
+#   extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+#    CORS_ALLOWED_ORIGIN_REGEXES = [
+#       rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+#    ]
 
 # CORS_ALLOWED_ORIGINS = ["https://appy-families.herokuapp.com/"]
 CORS_ALLOW_CREDENTIALS = True
