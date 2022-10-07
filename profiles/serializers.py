e0d9a5a37dc2d7c780e36353cd257acd471be64b
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-07 20:18:29 +0000

    add the get following id in the profile serializers so users can see who's following

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,7 @@
 ''' serializers for profile app '''
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 # class taken from DRF-APi walkthrough with modifications
@@ -8,16 +9,28 @@ class ProfileSerializer(serializers.ModelSerializer):
     '''profile serializer class'''
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         ''' check user is owner '''
         request = self.context['request']
         return request.user == obj.owner
 
+
+    def get_following_id(self, obj):
+        '''return following count'''
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         '''fields we want to display'''
         model = Profile
         fields = [
             'id', 'owner', 'created_on', 'name', 'bio',
-            'image', 'is_owner',
+            'image', 'is_owner', 'following_id'
         ]
