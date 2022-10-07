3068d5401db25e773c2a551a09ef13e3c810281b
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-07 19:50:09 +0000

    create the views file for list and detail views for followers

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+"""views for followers"""
+from rest_framework import generics, permissions
+from p5_drf_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+# taken from the drf walkthrough
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """followers list view"""
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        """make sure its the owner """
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """details of followers"""
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
